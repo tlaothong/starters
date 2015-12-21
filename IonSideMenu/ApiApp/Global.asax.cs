d65fffc41faca6b250d02bb8cb1ef1f817f12790
Teerachai Laothong <teerachai@perfenterprise.com>
2015-12-22 06:10:35 +0700

    Add an ApiApp.
    Signed-off-by: Teerachai Laothong <teerachai@perfenterprise.com>

---
+++ IonSideMenu/ApiApp/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace ApiApp
+{
+    /// <summary>
+    /// An application startup
+    /// </summary>
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Invokes when start the application.
+        /// </summary>
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
