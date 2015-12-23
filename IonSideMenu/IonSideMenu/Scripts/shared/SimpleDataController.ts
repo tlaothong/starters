95ddc4368753e132dc2a1ef0edfdb9d62b773272
Teerachai Laothong <teerachai@perfenterprise.com>
2015-12-23 18:37:54 +0700

    Add MockTableDataService (module app.shared) in IonSideMenu.
    Signed-off-by: Teerachai Laothong <teerachai@perfenterprise.com>

---
+++ IonSideMenu/IonSideMenu/Scripts/shared/SimpleDataController.ts
@@ -0,0 +1,20 @@
+﻿module app.shared {
+	'use strict';
+
+	class SimpleDataController {
+
+		//public model: any = null;
+
+        static $inject = ['data', '$scope', 'app.shared.SampleDataService'];
+        constructor(public data, private $scope: ng.IScope, public SampleDataService: IMockDataService) {
+		}
+
+		// public myMethod(): void {
+		// }
+
+	}
+
+	angular
+		.module('app.shared')
+		.controller('app.shared.SimpleDataController', SimpleDataController);
+}
